5162ddceab854902568422283781c40e13902201
nirav33 <niravparmar11d@gmail.com>
2024-02-15 17:23:19 +0530

    final

--- Registrastion Form/register.js
+++ Registrastion Form/register.js
@@ -1,11 +1,27 @@
-const signup = document.querySelector('.t-signup');
-const login = document.querySelector('.t-login');
-const addclass = document.querySelector('.site');
+let btn = document.getElementById("menu-btn");
+btn.onclick = () => {
+  document.getElementById("nav-menu-mobile").style.display = "flex";
+};
+document.getElementById("menu-close-btn").onclick = () => {
+  document.getElementById("nav-menu-mobile").style.display = "none";
+};
 
-signup.addEventListener('click', function(){
-    addclass.className = 'site signup-show';
-});
 
-login.addEventListener('click', function(){
-    addclass.className = 'site login-show';
-});
+
+// function showLoginPopup() {
+//   window.location.href='Registrastion Form/Login.html';        
+// }
+
+function closeLoginPopup() {
+    document.getElementById("loginPopup").style.display = "none";
+}
+
+function showDropdown() {
+    var dropdownContent = document.querySelector('.dropdown-content');
+    dropdownContent.style.display = 'block';
+}
+
+function hideDropdown() {
+    var dropdownContent = document.querySelector('.dropdown-content');
+    dropdownContent.style.display = 'none';
+}
